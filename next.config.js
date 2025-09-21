702b956746171c5c61ffa8a7a4167884cecaef23
syrlramadhan <syahrul0422@gmail.com>
2025-09-21 15:49:02 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
+  // Explicit path mapping untuk memastikan resolusi bekerja di server
+  experimental: {
+    // Membantu dengan resolusi module
+    externalDir: true,
+  },
   images: {
-    domains: ['192.168.1.22', 'localhost'],
+    domains: ['192.168.1.22', 'localhost', 'autoclean.pusatweb.cloud'],
     remotePatterns: [
       {
         protocol: 'http',
@@ -12,6 +17,11 @@ module.exports = {
         protocol: 'http',
         hostname: 'localhost',
         port: '8080',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'autoclean.pusatweb.cloud',
         pathname: '/**',
       },
     ],
